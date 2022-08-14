7763e1b79803ea9f2510ece8329d98e3c666ee6c
Steve Kong <mookie@somebox.org>
2022-08-14 16:00:07 -0700

    initial

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # hugo-dump-to-html
+
+Brute force Python script to read all the Hugo files for my movie reviews and dump them into a single HTML file for formatting and future use.
---
+++ createDocs.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+import pprint
+import yaml
+import markdown
+import os
+
+reviewsDir = "../sample/"
+documentTitle = "Movie Reviews by Steve Mookie Kong"
+finalDocumentFile = "./movie-reviews-all.html"
+
+def create_header(documentTitle):
+    header = "<html><head><title>" + documentTitle + "</title></head><body>"
+    return(header)
+
+def create_footer():
+    footer = "</body></html>"
+    return(footer)
+
+def create_review(file):
+    with open(file,"r") as mdfile:
+        mytext = mdfile.read()
+        mysections = mytext.split('---')
+
+    header = mysections[1]
+    body = mysections[2]
+
+    meta = yaml.safe_load(header)
+
+    reviewTitle = meta['title'] + " (" + str(meta['release_year']) + ")"
+    reviewDate = meta['date'].strftime("%B %d, %Y")
+
+    bodyNoPics = "<h1 id=\"Movie Title\">" + reviewTitle + "</h1>\n"
+    bodyNoPics = bodyNoPics + "<p id=\"Review Publication Date\"><small><b>" + reviewDate + "</b></small></p>\n"
+
+    if('rating' in meta):
+        movieRating = "<b>" + str(meta['rating']) + "</b><i> out of 10</i>"
+        bodyNoPics = bodyNoPics + "<p id=\"Review Rating\"><small>" + movieRating + "</small></p>\n"
+
+    for line in body.split("\n"):
+        if (not line.startswith("![")):
+            bodyNoPics = bodyNoPics + line + "\n"
+
+    bodyNoPics = bodyNoPics + "<hr />"
+    bodyHtml = markdown.markdown(bodyNoPics)
+    return(bodyHtml)
+    
+def writeFinalDocument(finalDocumentFile,finalDocument):
+    doc = open(finalDocumentFile,"w")
+    doc.write(finalDocument)
+    doc.close()
+
+def main():
+
+    documentHeader = create_header(documentTitle)
+    documentFooter = create_footer()
+
+    documentBody = ""
+    for file in os.listdir("../sample/"):
+        documentBody = documentBody + create_review(reviewsDir + file)
+
+    finalDocument = documentHeader + documentBody + documentFooter
+
+    writeFinalDocument(finalDocumentFile,finalDocument)
+
+if __name__ == "__main__":
+    main()
